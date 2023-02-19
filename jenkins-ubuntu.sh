3ec8dd53d39de36b0c7c55dbb3b656a0017853b0
jeanne <jeannedinangue@gmail.com>
2023-02-18 19:45:58 -0600

    agent script

--- jenkins-ubuntu.sh
+++ jenkins-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Author:  afhndsfjhdsgvjb@gmail.com
+#Author: jeannedinangue@yahoo.fr
 #Date: feb 2023
 #Description: Installation of jenkins on ubuntu
 
@@ -15,25 +15,3 @@ sudo apt-get install jenkins -y
 sudo apt install git -y
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
-  
-*** Docker-install.sh
-
-#!/bin/bash
-# Created by Serge
-# Install docker in centos
-
-# Remove any old versions
-sudo yum remove docker docker-common docker-selinux docker-engine
-
-# Install required packages
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-
-# Configure docker repository
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
-# Install Docker-ce
-sudo yum install docker-ce -y
-
-# Start Docker
-sudo systemctl start docker
-sudo systemctl enable docker
